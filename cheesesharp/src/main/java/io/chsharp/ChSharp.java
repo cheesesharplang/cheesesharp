b4e0005b9b263ce58457fb8b149cf11e382c5faf
lvivtotoro <7090833+lvivtotoro@users.noreply.github.com>
2018-06-03 14:14:50 +0300

    Updated test.

--- cheesesharp/src/main/java/io/chsharp/ChSharp.java
+++ cheesesharp/src/main/java/io/chsharp/ChSharp.java
@@ -3,19 +3,20 @@ package io.chsharp;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
+import io.chsharp.ast.NodeCompilationUnit;
 import io.chsharp.lexpar.Lexer;
-import io.chsharp.lexpar.Token;
+import io.chsharp.lexpar.Parser;
+import io.chsharp.visitor.VisitorSemanticAnalyzer;
 
 public class ChSharp {
 	
 	public static void main(String[] args) {
-		String testInput = "cheese A was Brie make end 0.005 \"AH\"";
+		String testInput = "Gouda j is 9\nj is 3";
 		Lexer l = new Lexer(new ByteArrayInputStream(testInput.getBytes(StandardCharsets.UTF_8)));
+		Parser p = new Parser(l);
 		
-		Token t;
-		while ((t = l.nextToken()).type != Token.Type.EOF) {
-			System.out.println(t);
-		}
+		NodeCompilationUnit cunit = p.matchCUnit();
+		cunit.accept(new VisitorSemanticAnalyzer());
 	}
 	
 }
